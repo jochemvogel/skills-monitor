4700568928e616ad115d81e793473d02e8f9138d
David Aspeslagh <aspe0004@hz.nl>
2018-03-21 15:35:36 +0100

    Changed text in profile.index to english and changed the layout a bit.

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -2,23 +2,26 @@
 <?php use Illuminate\Support\Facades\Auth; ?>
     @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-                <h1>Profiel pagina</h1>
-            </div>
-        </div>
 
         <!-- Shows information about the user when logged in -->
 
         @if(Auth::check())
             <div class="row">
-                <div class="col-sm-5">
-                    <p>Welkom <?php echo(Auth::user()->firstname.' '.Auth::user()->lastname); ?></p>
-                </div>
-        
-                <div class="col-sm-8">
-                    <p>Je email adres is: <?php echo(Auth::user()->email); ?>
-                    Wil je dit veranderen? Klik dan <a href="users.index">hier</a>  </p>
+                <div class="col-md-6">
+                    <div class="box box-solid">
+
+                        <div class="box-header with-border">
+                            <i class="fa fa-user"></i> 
+                            <h1 class="box-title">Profile</h3>
+                        </div>
+
+                        <div class="box-body">
+                            <h3>Welcome <?php echo(Auth::user()->firstname.' '.Auth::user()->lastname); ?></h2>
+                            <p>Your e-mail address is: <?php echo(Auth::user()->email); ?></p>
+                            <p>Do you want to change your email address? Click <a href="users.index">here</a>  </p>
+                        </div>
+
+                    </div>
                 </div>
             </div>
          @endif
@@ -26,7 +29,7 @@
         <!-- When not logged in user is redirected to login page -->
 
          @if(Auth::guest())
-         <a href="/login" class="btn btn-info"> Je moet eerst inloggen om je profiel te kunnen zien! >></a>
+         <a href="/login" class="btn btn-danger"> You have to login first before you can view your profile!</a>
          @endif
     </div>
     @endsection
