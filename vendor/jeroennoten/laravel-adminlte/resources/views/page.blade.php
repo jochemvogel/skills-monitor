e311454f16c312ddd7b725d20a869fecc04a1672
mcDoggyKiip <eijk0037@hz.nl>
2018-04-04 11:12:35 +0200

    fix header image not showing up

--- vendor/jeroennoten/laravel-adminlte/resources/views/page.blade.php
+++ vendor/jeroennoten/laravel-adminlte/resources/views/page.blade.php
@@ -53,7 +53,7 @@
                                             <a href="#">
                                                 <div class="pull-left">
                                                     <!-- User Image -->
-                                                    <img src="img/coffee.png" class="img-circle" alt="User Image">
+                                                    <img src="{{ URL::to('/') }}/img/coffee.png" class="img-circle" alt="User Image">
                                                 </div>
                                                 <!-- Message title and timestamp -->
                                                 <h4>
@@ -105,14 +105,14 @@
                             <!-- Menu Toggle Button -->
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <!-- The user image in the navbar-->
-                                <img src="img/coffee.png" class="user-image" alt="User Image">
+                                <img src="{{ URL::to('/') }}/img/coffee.png" class="user-image" alt="User Image">
                                 <!-- hidden-xs hides the username on small devices so only the image appears. -->
                                 <span class="hidden-xs">{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</span>
                             </a>
                             <ul class="dropdown-menu">
                                 <!-- The user image in the menu -->
                                 <li class="user-header">
-                                    <img src="img/coffee.png" class="img-circle" alt="User Image">
+                                    <img src="{{ URL::to('/') }}/img/coffee.png" class="img-circle" alt="User Image">
 
                                     <p>
                                         Admin
