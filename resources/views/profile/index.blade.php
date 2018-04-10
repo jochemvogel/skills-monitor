a0f3199034d5059f2eaecbdfd1782ae6bb061f51
David Aspeslagh <aspe0004@hz.nl>
2018-04-10 14:09:18 +0200

    initial commit

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,7 +1,6 @@
 @extends('adminlte::page')
-<?php use Illuminate\Support\Facades\Auth; ?>
+{{-- use Illuminate\Support\Facades\Auth; --}}
     @section('content')
-    <div class="container">
 
         <!-- Shows information about the user when logged in -->
 
@@ -16,8 +15,8 @@
                         </div>
 
                         <div class="box-body">
-                            <h3>Welcome <?php echo(Auth::user()->firstname.' '.Auth::user()->lastname); ?></h2>
-                            <p>Your e-mail address is: <?php echo(Auth::user()->email); ?></p>
+                            <h3>Welcome {{-- echo(Auth::user()->firstname.' '.Auth::user()->lastname));  --}}</h2>
+                            <p>Your e-mail address is: {{-- Auth::user()->email); --}}</p>
                             <p>Do you want to change your email address? Click <a href="{{ route('users.index') }}">here</a></p>
                         </div>
 
@@ -31,6 +30,6 @@
          @if(Auth::guest())
          <a href="/login" class="btn btn-danger"> You have to login first before you can view your profile!</a>
          @endif
-    </div>
+         
     @endsection
 
