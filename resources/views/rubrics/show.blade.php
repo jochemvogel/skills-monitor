66cdc9e0041321d2e79524cd51c0b5e99162033e
Bradley Eijke <eijk0037@hz.nl>
2018-04-13 13:31:41 +0200

    fix add and remove row button

--- resources/views/rubrics/show.blade.php
+++ resources/views/rubrics/show.blade.php
@@ -33,21 +33,21 @@
                     @method('PUT')
                     @csrf
                     <input type='hidden' name='name' value='{{ $rubrics->name}}'>
-                    <input type='hidden' name='rows' value='{{ $rubrics->rows + 1 }}'>
+                    <input type='hidden' name='rows' value='{{ $rows + 1 }}'>
                     <input type='hidden' name='cols' value='{{ $rubrics->cols }}'>
-                    <button class='btn-info btn-sm' type='submit' name='add_row'><i class='fa fa-plus-square-o'></i>&nbsp;&nbsp;Voeg rij toe</button>
+                    <button class='btn-info btn-sm' type='submit' name='add_row'><i class='fa fa-plus-square-o'></i>&nbsp;&nbsp;Add row</button>
                 </form>
             </div>
             <div class="pull-right">
                 {{-- remove row button --}}
-                @if(session('rows') > 1)
-                    <form method='POST' action="{{route('rubrics.update',['id' => session('id')])}}">
+                @if($rows > 1)
+                    <form method='POST' action="{{route('rubrics.update',['id' => $rubrics->id])}}">
                         @method('PUT')
-                        <input type='hidden' name='_token' value='{{ Session::token() }}'>
-                        <input type='hidden' name='name' value='{{ session('name') }}'>
-                        <input type='hidden' name='rows' value='{{ session('rows') - 1 }}'>
-                        <input type='hidden' name='cols' value='{{ session('cols') }}'>
-                        <button class='btn-danger btn-sm' type='submit' name='delete_row'><i class='fa fa-minus-square-o'></i>&nbsp;&nbsp;Verwijder rij</button>
+                        @csrf
+                        <input type='hidden' name='name' value='{{ $rubrics->name }}'>
+                        <input type='hidden' name='rows' value='{{ $rows - 1 }}'>
+                        <input type='hidden' name='cols' value='{{ $rubrics->cols }}'>
+                        <button class='btn-danger btn-sm' type='submit' name='delete_row'><i class='fa fa-minus-square-o'></i>&nbsp;&nbsp;Remove row</button>
                     </form>
                 @endif
             </div>
