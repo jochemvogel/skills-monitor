c965508a587a6965dbecea1160fc2656a90e347f
mcDoggyKiip <eijk0037@hz.nl>
2018-04-13 15:26:51 +0200

    try to fix .bat not working properly

--- laatste.bat
+++ laatste.bat
@@ -2,8 +2,10 @@ cd vendor
 rmdir /s /q jeroennoten
 cd ..
 composer require jeroennoten/laravel-adminlte
+composer dump-autoload
 git stash
 git merge --abort
 git checkout master
 git pull
 php artisan migrate:fresh --seed
+pause
