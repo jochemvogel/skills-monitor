533aeaad839702bd8e3be955587b8e50711bb40d
David Aspeslagh <aspe0004@hz.nl>
2018-04-13 10:50:40 +0200

    Changed to match the master branch

---
+++ resources/views/inbox/inbox.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('content-header')
+    <h1>
+        Mailbox
+        <small>13 new messages</small>
+    </h1>
+    
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Mailbox</li>
+      </ol>
+@endsection
+
+@section('content')
+
+@endsection
