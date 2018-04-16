69e43f5cdba98027a3387eb35632d0a705056fbb
Adit Permadi <perm0002@hz.nl>
2018-04-16 11:05:32 +0200

    working buttons

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -10,7 +10,22 @@ class StatsController extends Controller
 
 public function index()
 {
-    return view('progress.index');
+    return view('stats.index');
+}
+
+public function blok2()
+{
+    return view('stats.blok2');
+}
+
+public function blok3()
+{
+    return view('stats.blok3');
+}
+
+public function blok4()
+{
+    return view('stats.blok4');
 }
 
 }
---
+++ resources/views/stats/blok2.blade.php
@@ -0,0 +1,56 @@
+@extends('adminlte::page')
+
+@section('content')
+
+    <div class="box box-solid">
+        <div class="box-header with-border">
+            <h3 class="box-title">
+                <strong>
+                    Progress
+                </strong>
+            </h3>
+        </div>
+
+        <div class="box-body">
+            <a href="" class="btn btn-primary btn-xs">BLOK 1</a>
+            <a href="{{route('stats.blok2')}}" class="btn btn-primary btn-xs">BLOK 2</a>
+            <a href="{{route('stats.blok3')}}" class="btn btn-primary btn-xs">BLOK 3</a>
+            <a href="{{route('stats.blok4')}}" class="btn btn-primary btn-xs">BLOK 4</a>
+        
+            <div class="x_content" id="table">  
+                <table id="datatable-buttons" class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Course</th>
+                            <th>Grade</th>
+                            <th>EC</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                            <!-- Foreach-loop, create table row with data for each course-->
+                            <tr>
+                                <td>BBBBB</td>
+                                <td>##</td>
+                                <td>##</td>    
+                            </tr>
+
+                    </tbody>    
+
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@push('js')
+<script type='text/javascript'>
+    function addListeners() {
+        document.getElementById('1').onclick = function(){alert('Hello');}
+    }
+
+
+    window.onload = addListeners;
+</script>
+@endpush
