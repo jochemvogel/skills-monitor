4277f9ffd5c6e83b0cb9e88ce9d055acd961d9af
Adit Permadi <perm0002@hz.nl>
2018-04-16 09:26:41 +0200

    controller modified

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,5 @@ Route::get('/profile', 'profileController@show')->name('profile');
 
 Route::resource('users', 'usersController');
 Route::resource('rubrics', 'rubricsController');
-<<<<<<< HEAD
 Route::resource('stats', 'StatsController');
-=======
 
-Route::post('/rubrics/create/part2', 'rubricsController@create2')->name('rubrics.create.part2');
->>>>>>> b152a958a02f5f8b66ce6efb7646667f5e34f300
