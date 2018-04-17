3bb46d67338f3e80fbfdb9c8ea2ab925fc38cb9e
LAPTOP-QPPSB29C\rspuy <r.spuy1@gmail.com>
2018-04-17 12:11:43 +0200

    changed CourseTableSeeder

--- database/seeds/CourseTableSeeder.php
+++ database/seeds/CourseTableSeeder.php
@@ -6,15 +6,35 @@ class CourseTableSeeder extends Seeder
 {
   public function run()
   {
-    $role_admin = Role::where('name', 'admin')->first();
-    
-    $admin = new User();
-    $admin->firstname = 'Admin';
-    $admin->lastname = 'Admin';
-    $admin->email = 'Admin@Admin.com';
-    $admin->password = bcrypt('123456');
-    $admin->save();
-    $admin->roles()->attach($role_admin);   
+    $course1 = new Course();
+    $course1->name = 'Software Ontwerpen 1';
+    $course1->course_abbreviation = 'SON1';
+    $course1->course_code = 'CU19638';
+    $course1->save();
+
+    $course2 = new Course();
+    $course2->name = 'Software Beheren 1';
+    $course2->course_abbreviation = 'SBE1';
+    $course2->course_code = 'CU19639';
+    $course2->save();
+
+    $course3 = new Course();
+    $course3->name = 'Infrastructuur Realiseren 1';
+    $course3->course_abbreviation = 'IRE1';
+    $course3->course_code = 'CU19641';
+    $course3->save();
+
+    $course4 = new Course();
+    $course4->name = 'Werk- en denkniveau Agile 2a';
+    $course4->course_abbreviation = 'WEDAG2a';
+    $course4->course_code = 'CU22126';
+    $course4->save();
+
+    $course5 = new Course();
+    $course5->name = 'Werk- en denkniveau English 2a';
+    $course5->course_abbreviation = 'WEDEN2a';
+    $course5->course_code = 'CU22541';
+    $course5->save();
   }
 }
 
