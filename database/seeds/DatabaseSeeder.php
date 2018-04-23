bd08df51bd4a9eddf27a6aa530c8bdf43fe23f08
Adit Permadi <perm0002@hz.nl>
2018-04-23 11:48:20 +0200

    mergerd for real

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,10 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RubricsTableSeeder::class);
         $this->call(RowsTableSeeder::class);
         $this->call(FieldsTableSeeder::class);
-<<<<<<< HEAD
         $this->call(ResultsTableSeeder::class);
-=======
         $this->call(CourseTableSeeder::class);
->>>>>>> b4ce17389e4f224e5d3845601b9710772e23b460
     }
 }
