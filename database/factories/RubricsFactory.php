8e0ea41fcf7f043f4d3a3857d0ace0f93d28d1b2
Jochem <voge0031@hz.nl>
2018-04-23 15:26:06 +0200

    Added random course names

--- database/factories/RubricsFactory.php
+++ database/factories/RubricsFactory.php
@@ -7,7 +7,7 @@ $factory->define(Rubrics::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence,
         'cols' => rand(1,10),
-        'course_id' => 1,
+        'course_id' => rand(1,5),
         'user_id' => 1,
     ];
 });
