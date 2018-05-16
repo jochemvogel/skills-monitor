8d8e1015bad978971fa3645823508961f9b0a876
Bradley Eijke <eijk0037@hz.nl>
2018-05-16 10:07:50 +0200

    "make edit function work again (wrong branch sorry)"

--- resources/views/rubrics/edit.blade.php
+++ resources/views/rubrics/edit.blade.php
@@ -1,96 +1,169 @@
 @extends('adminlte::page')
 
-@section('title', 'Rubrics: Edit')
+@section('title', 'Rubrics: ' . $rubrics->name)
 
 @section('content')
+    {{-- create a box around the rubrics and it's name --}}
     <div class="box box-solid">
-        <div class="box-header">
+        <div class="box-header with-border">
             <h1 class="box-title">
                 <strong>
-                    Rubrics: Edit
+                    {{-- show the name of the rubrics --}}
+                    {{ $rubrics->name }}
                 </strong>
-                <a href="{{route('rubrics.index')}}" class="btn btn-info btn-xs" title="Back">
-                    <i class="fa fa-chevron-left"></i> Back
-                </a>
             </h1>
         </div>
-
         <div class="box-body">
-            <div id="example1_wrapper" class="">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="dataTables_length" id="example1_length">
-                        </div>
-                    </div>
-                </div>
-
-                {{-- FORM --}}
-                <form class="form-horizontal" action="{{ route('rubrics.update', ['id' => $rubric->id]) }}" method="put">
-
-
-                    {{-- NAME--}}
-
-                    <input type='hidden' name='rubric' value='1'>
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="name" class="col-sm-2 control-label">Name <span class="required">*</span></label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" name="rubric_name" value="{{$rubric->name}}" id="rubric_name">
-                            </div>
-                        </div>
-
-
-                        {{-- END NAME --}}
-
-
-
-                        {{--DROPDOWN--}}
-
-                        <div class="form-group {{ $errors->has('course') ? ' has-error ' : '' }}">
-                            <label class="col-sm-2 control-label" for="course">Course <span class="required">*</span></label>
-                            <div class="col-sm-10">
-                                <select class="form-control select2 select2-hidden-accessible" data-placeholder="Select a course" style="width: 100%;" tabindex="-1" aria-hidden="true" name="course">
-                                    @if ($courses->count())
-                                        @foreach($courses as $course)
-                                            <option value="{{ $course->id }}">{{ $course->name }}</option>
-                                        @endforeach
-                                    @endif
-                                </select>
-                            </div>
-                            @if ($errors->has('course'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('course') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-
-
-                        {{--END DROP DOWN-- }}
-
-
-                        {{-- SUBMIT BUTTON --}}
-                        <div class="form-group">
-                            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                                <input type='submit' class='btn btn-success btn-sm' value='Save'>
-                            </div>
-                        </div>
-                    </div>
-
-                    {{-- END SUBMIT BUTTON --}}
-                </form>
-
-                {{-- END FORM --}}
-            </div>
+            {{-- make a table with the selected number of columns and rows --}}
+            <table class='table table-striped table-bordered'>
+                <thead>
+                </thead>
+                <tbody id="tbody">
+                @foreach($rubrics->rowobjects as $row)
+                    <tr id="row_{{$row->id}}">
+                        @can('update', $rubrics)
+                            <td>
+                                @if($rubrics->rowobjects->first()->id != $row->id)
+                                    <i class="fa fa-toggle-up"></i>
+                                    <br />
+                                @endif
+                                <i class="fa fa-trash"></i>
+                                @if($rubrics->rowobjects->last()->id != $row->id)
+                                    <br />
+                                    <i class="fa fa-toggle-down"></i>
+                                @endif
+                            </td>
+                        @endcan
+                        @foreach($row->fields as $field)
+                            <td id="field_{{ $field->id }}">
+                                {{  $field->content }}
+                            </td>
+                        @endforeach
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
 
 @push('js')
-    <script>
-        $(document).ready(function() {
-            $('.select2').select2();
-        });
-    </script>
+    @can('update', $rubrics)
+        <script>
+            document.addEventListener('click', function(){
+                var elem = event.srcElement;
+                var row = elem.parentElement.parentElement;
+                console.log(row.id + "," + elem.id);
+                if(row.id.substr(0,3) == "row"){
+                    var rowid = row.id.substr(4);
+                    switch(elem.classList[1]){
+                        case "fa-toggle-up":
+                            console.log("up");
+                            $.ajax({
+                                headers: {
+                                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                },
+                                type: "GET",
+                                url: '/moverow',
+                                data: {
+                                    'id': rowid,
+                                    'move': 'up',
+                                },
+                                success: function(data) { window.location.replace("/rubrics/"+data+"/edit"); }
+                            });
+                            break;
+                        case "fa-trash":
+                            console.log("delete");
+                            //#TODO add ajax call to delete row
+                            break;
+                        case "fa-toggle-down":
+                            console.log("down");
+                            $.ajax({
+                                headers: {
+                                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                },
+                                type: "GET",
+                                url: '/moverow',
+                                data: {
+                                    'id': rowid,
+                                    'move': 'down',
+                                },
+                                success: function(data) { window.location.replace("/rubrics/"+data+"/edit"); }
+                            });
+                            break;
+                    }
+                }else if(elem.id.substr(0,5) == "field"){
+                    var field = document.getElementById(elem.id);
+                    var content = field.innerText;
+                    // remove Save from content
+                    if (content.substr(content.length - 4) == "Save") {
+                        content = content.substr(0, content.length - 5);
+                    }
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        type: "GET",
+                        url: '/getpending',
+                        data: {
+                            'id': field.id.substr(6),
+                        },
+                        success: function (data) {
+                            field.innerHTML = "<div id='newText' contenteditable='true'>" + data + "</div>";
+                            field.innerHTML += "<div id='oldText' class='hidden'>" + content + "</div>"
+                            field.innerHTML += "<button class='btn btn-success' id='safe'>Save</button>";
+                            document.getElementById("newText").focus();
+                        },
+                        error: function () {
+                            field.innerHTML = "<div id='newText' contenteditable='true'>" + content + "</div>";
+                            field.innerHTML += "<div id='oldText' class='hidden'>" + content + "</div>"
+                            field.innerHTML += "<button class='btn btn-success' id='safe'>Save</button>";
+                            document.getElementById("newText").focus();
+                        }
+                    });
+                }
+            });
+
+            document.addEventListener('focusout', function (){
+                var field = event.srcElement.parentElement;
+                var oldContent = document.getElementById("oldText").innerHTML;
+                var newContent = document.getElementById("newText").innerHTML;
+                if (event.relatedTarget) {
+                    if(event.relatedTarget.id == "safe"){
+                        field.innerHTML = newContent;
+                        $.ajax({
+                            headers: {
+                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                            },
+                            type: "PUT",
+                            url: '/updatefield',
+                            data: {
+                                'id': field.id.substr(6),
+                                'content': newContent,
+                            },
+                            success: function() { console.info('success')}
+                        });
+                    }else{
+
+                    }
+                }else{
+                    field.innerHTML = oldContent;
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        type: "PUT",
+                        url: '/backupfield',
+                        data: {
+                            'id': field.id.substr(6),
+                            'content': newContent,
+                        },
+                        success: function() { console.info('success')}
+                    });
+                }
+            });
+        </script>
+    @endcan
 @endpush
 
 @push ('css')
