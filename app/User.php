48fd8f9503dc9a7273731da659fd87a40d88b56c
Jochem <voge0031@hz.nl>
2018-05-21 17:44:14 +0200

    Created a table for the many to many relation of Courses & Users

--- app/User.php
+++ app/User.php
@@ -33,10 +33,10 @@ class User extends Authenticatable
     public function authorizeRoles($roles)
     {
       if (is_array($roles)) {
-          return $this->hasAnyRole($roles) || 
+          return $this->hasAnyRole($roles) ||
                  abort(401, 'This action is unauthorized.');
       }
-      return $this->hasRole($roles) || 
+      return $this->hasRole($roles) ||
              abort(401, 'This action is unauthorized.');
     }
     /**
@@ -58,5 +58,10 @@ class User extends Authenticatable
 
     public function Role(){
         return $this->belongsTo('App\Role');
+    }
+
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class);
     }
 }
