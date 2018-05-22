5ce3b03b1e89d445f4826215fee96d0adf5cea2b
LAPTOP-QPPSB29C\rspuy <r.spuy1@gmail.com>
2018-05-22 11:31:22 +0200

    changed lay-out for the delete pages

--- resources/views/courses/delete.blade.php
+++ resources/views/courses/delete.blade.php
@@ -3,20 +3,16 @@
 @section('title', 'Courses: Delete')
 
 @section('content')
-    @section('content_header')
-    @endsection
-    <div class="">
-        <div class="clearfix"></div>
-
-        <div class="row">
-            <div class="col-md-12">
-                <div class="x_panel">
-                    <div class="x_title">
-                        <h2>Confirm Delete Record <a href="{{route('courses.index')}}" class="btn btn-info btn-xs"><i class="fa fa-chevron-left"></i> Back </a></h2>
-                        <div class="clearfix"></div>
+        <div class="box box-solid">
+            <div class="box-header with-border">
+                        <h1 class="box-title">
+                            <strong>
+                                Confirm Delete Course <a href="{{route('courses.index')}}" class="btn btn-primary btn-xs"><i class="fa fa-chevron-left"></i> Back </a>
+                            </strong>
+                        </h1>
                     </div>
-                    <div class="x_content">
-                        <p>Are you sure you want to delete <strong>{{$course->name}}</strong></p>
+                    <div class="box-body">
+                        <p>Are you sure you want to delete <strong>{{$course->name}}</strong>?</p>
 
                         <form method="POST" action="{{ route('courses.destroy', ['id' => $course->id]) }}">
                             <input type="hidden" name="_token" value="{{ Session::token() }}">
@@ -27,7 +23,6 @@
                 </div>
             </div>
         </div>
-    </div>
 @endsection
 
 @push ('css')
--- resources/views/rubrics/delete.blade.php
+++ resources/views/rubrics/delete.blade.php
@@ -3,18 +3,16 @@
 @section('title', 'Rubrics: Delete')
 
 @section('content')
-<div class="">
-    <div class="clearfix"></div>
-
-    <div class="row">
-        <div class="col-md-12">
-            <div class="x_panel">
-                <div class="x_title">
-                    <h2>Confirm Delete Record <a href="{{route('rubrics.index')}}" class="btn btn-info btn-xs"><i class="fa fa-chevron-left"></i> Back </a></h2>
-                    <div class="clearfix"></div>
+<div class="box box-solid">
+        <div class="box-header with-border">
+                    <h1 class="box-title">
+                        <strong>
+                            Confirm Delete Rubric <a href="{{route('rubrics.index')}}" class="btn btn-primary btn-xs"><i class="fa fa-chevron-left"></i> Back </a>
+                        </strong>
+                    </h1>
                 </div>
-                <div class="x_content">
-                    <p>Are you sure you want to delete <strong>{{$rubric->name}}</strong></p>
+                <div class="box-body">
+                    <p>Are you sure you want to delete <strong>{{$rubric->firstname}} {{$rubric->lastname}}</strong>?</p>
 
                     <form method="POST" action="{{ route('rubrics.destroy', ['id' => $rubric->id]) }}">
                         <input type="hidden" name="_token" value="{{ Session::token() }}">
@@ -25,7 +23,6 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @push ('css')
