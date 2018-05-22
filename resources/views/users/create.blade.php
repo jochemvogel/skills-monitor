6ccf1a22bbd6610975171a8eb057c5fdb974b63a
LAPTOP-QPPSB29C\rspuy <r.spuy1@gmail.com>
2018-05-22 10:46:00 +0200

    added error handler for the user create page

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -27,10 +27,13 @@
                         <input type="text" class="form-control" name="lastname" placeholder="last name" required>
                     </div>
                 </div>
-                <div class="form-group"{{ $errors->has('email') ? ' has-error' : '' }}>
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                     <label for="Email" class="col-sm-2 control-label">Email</label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" name="email" placeholder="email" required>
+                        @if ($errors->has('email'))
+                        <span class="help-block">{{ $errors->first('email') }}</span>
+                        @endif
                     </div>
                 </div>
                 <div class="form-group"{{ $errors->has('password') ? ' has-error' : '' }}>
@@ -47,7 +50,7 @@
                 </div>
 
                 <div class="form-group {{ $errors->has('role') ? ' has-error ' : '' }}">
-                    <label class="col-sm-2 control-label" for="email">Role <span class="required">*</span>
+                    <label class="col-sm-2 control-label" for="role">Role <span class="required">*</span>
                     </label>
                     <div class="col-sm-10">
                         <select class="form-control select2" name="role" id="role">
