6c1db96ba6d173312b0305473741d386d8f6b915
Robin <r.spuy1@gmail.com>
2018-06-06 08:35:24 +0000

    confirm.blade.php edited

--- resources/views/mail/confirm.blade.php
+++ resources/views/mail/confirm.blade.php
@@ -1,6 +1,6 @@
-    @component('mail::message')
-        An account has been made for you!
-    @endcomponent
-    @component('mail::button', ['url' => env('APP_URL')."/password/reset/".$token])
-        Set password
-    @endcomponent
+@component('mail::message')
+    An account has been made for you!
+@endcomponent
+@component('mail::button', ['url' => env('APP_URL')."/password/reset/".$token])
+    Set password
+@endcomponent
