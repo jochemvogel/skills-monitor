e78ff7ab738850399d1b1eb180da0b95e66fd0b4
LAPTOP-QPPSB29C\rspuy <r.spuy1@gmail.com>
2018-06-06 10:19:01 +0200

    Changed dashboard back to the master version, had changed it because of GRE1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,11 @@
 @extends('adminlte::page')
 
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 @section('content')
-<div class="box box-solid" style="width: 17%;">
-    <div class="box-header with-border">
-        <h1 class="box-title">
-            <strong>
-                Users in this course
-            </strong>
-        </h1>
-    </div>
-    <div class="box-body">
-        <p><i class="fa fa-circle" style="color: green;"></i> David Aspelagh</p>
-        <p><i class="fa fa-circle" style="color: red;"></i> Bradley Eijke</p>
-        <p><i class="fa fa-circle" style="color: red;"></i> Adit Permadi</p>
-        <p><i class="fa fa-circle" style="color: green;"></i> Robin Spuij</p>
-        <p><i class="fa fa-circle" style="color: red;"></i> Jochem Vogel</p>
-    </div>
-    <div class="box-footer with-border">
-        <div class="pull-right">
-        <p style="font-size: 10px;"><i class="fa fa-circle" style="color: green;"></i> = online</p>
-        <p style="font-size: 10px;"><i class="fa fa-circle" style="color: red;"></i> = offline</p>
-        </div>
-    </div>
-</div>
+    <p>You are logged in!</p>
 @stop
 
 @push ('css')
