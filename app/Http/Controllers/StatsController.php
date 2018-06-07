4b3685d7e0e9e3acb7b0296216b3ca2b60164d69
Adit Permadi <perm0002@hz.nl>
2018-06-07 09:19:54 +0200

    StatsController modified. Comment code removed

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -9,14 +9,11 @@ use DB;
 class StatsController extends Controller {
 
     public function index() {
-
-        $results = DB::table('results')->where('blok', 1)->get();
+        $results = Results::with('course')->where('blok', 1)->get();
         return view('stats.index', ['results'=>$results]);
     }
 
     public function getDataBlok(Request $request) {
-
-        //$results = DB::table('results')->where('blok', '=' , $request->input("blok"))->get();
         $results =  Results::with('course')->where("blok","=",$request->input("blok"))->get();
         return $results;
     }
